69c6065415d06d65ac95291500db63076c517277
Maxine Michalski <maxine_red1@yahoo.com>
2018-11-12 10:52:18 +0100

    Moved server code into lib/

--- main.js
+++ main.js
@@ -17,38 +17,5 @@
  *  along with Alex.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const morgan = require('morgan');
-
-let app = express();
-
-let access_log = fs.createWriteStream('logs/access.log', { flags: 'a' });
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(morgan('combined', { stream: access_log }));
-
-let port = process.env.PORT || 3000;
-
-//let users = require('./routes/users');
-
-//app.use('/users', users);
-
-// Handle 404 responses properly.
-const ErrorController = require('./controllers/error');
-app.use(function (req, res) {
-  let err = new ErrorController(404);
-  res.status(err.code).json({error: err});
-});
-
-// Error handling
-app.use(function (err, req, res, next) { // eslint-disable-line no-unused-vars
-  if (process.env.NODE_ENV !== 'production') {
-    console.error(err.stack); // eslint-disable-line no-console
-  }
-  res.status(500).json({error: new Error(500)});
-});
-module.exports = app.listen(port);
+const server = require('./lib/server');
+module.exports = server;
