c7e939a84a6788ded820b3072af964cffd1da9fa
Maxine Michalski <maxine@furfind.net>
2018-11-28 14:12:05 +0100

    Added Memory
    Added a Memory class, that stores state, action pairs.
    
    There needs to be a way to link memories together or store state action
    pairs, that lead to the current one.

---
+++ test/memory.js
@@ -0,0 +1,89 @@
+/*
+ *  Copyright 2018 Maxine Michalski <maxine@furfind.net>
+ *
+ *  This file is part of Alex.
+ *
+ *  Alex is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Alex is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Alex.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+let chai = require('chai');
+let chai_array = require('chai-arrays');
+const Memory = require('../lib/memory');
+
+chai.use(chai_array);
+let expect = chai.expect;
+let memory = new Memory([0,0], [0]);
+
+
+describe('Memory', function () {
+  describe('new', function () {
+    it('returns a new class instance object', function (done) {
+      expect(new Memory([0,0], [0])).to.be.instanceOf(Memory);
+      done();
+    });
+    it('accepts an object instead of arrays', function (done) {
+      expect(new Memory({state: [0,0], action: [0], created_at: 1540000000})).to.be.instanceOf(Memory);
+      expect(new Memory(JSON.parse(JSON.stringify(memory)))).to.be.instanceOf(Memory);
+      expect(new Memory({state: [0,0], action: [0]})).to.be.instanceOf(Memory);
+      done();
+    });
+    it('throws an error if object misses state or action', function (done) {
+      expect(function () { new Memory({state: [0,0]}); }).to.throw(Error, 'memory objects require a state and action property');
+      expect(function () { new Memory({action: [0]}); }).to.throw(Error, 'memory objects require a state and action property');
+      done();
+    });
+    it('throws an error on undefined behavior', function (done) {
+      expect(function () { new Memory({state: [0,0], action: [0]}, [0]); }).to.throw(Error, 'unknown error');
+      done();
+    });
+    it('throws an error if neither state nor action is given', function (done) {
+      expect(function () { new Memory(); }).to.throw(Error, 'no state or action provided');
+      done();
+    });
+    it('throws an error if an action is expected but not given', function (done) {
+      expect(function () { new Memory([0, 0] ); }).to.throw(Error, 'action expected, but not provided');
+      done();
+    });
+    it('has a \'created_at\' property, that is a UNIX timestamp', function (done) {
+      expect(memory).to.have.property('created_at').and.be.above(1540000000);
+      done();
+    });
+    it('has a \'state\' property, that is an array of numbers', function (done) {
+      expect(memory).to.have.property('state');
+      expect(memory.state[0]).to.be.equal(0);
+      done();
+    });
+    it('has an \'action\' property, that is an array of numbers', function (done) {
+      expect(memory).to.have.property('action');
+      expect(memory.action[0]).to.be.equal(0);
+      done();
+    });
+    it('throws an error if the state size is incorrect', function (done) {
+      expect(function () { new Memory([0], [0]); }).to.throw(Error, 'State size mismatch:');
+      done();
+    });
+    it('throws an error if the state is not only made up of numbers', function (done) {
+      expect(function () { new Memory([[0,0],[0,0]], [0]); }).to.throw(Error, 'numbers only');
+      done();
+    });
+    it('throws an error if the action size is incorrect', function (done) {
+      expect(function () { new Memory([0, 0], [0,0]); }).to.throw(Error, 'Action size mismatch:');
+      done();
+    });
+    it('throws an error if the action is not only made up of numbers', function (done) {
+      expect(function () { new Memory([0, 0], [true]); }).to.throw(Error, 'numbers only');
+      done();
+    });
+  });
+});
